59e0ccb58fc8144f60e834c7f17f09f5f608dc3a
shrikant.jumbad <shrikant.jumbad@capgemini.com>
2026-06-25 18:31:31 +0530

    Intital Commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+# Addition Program in Python   
+# This program adds two numbers provided by the user and displays the result.
+#  
+
+num1 = 2
+num2 = 5
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
+print("Hello Wordld")
